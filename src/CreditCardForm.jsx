7bb976d43a4fe59b0b4a906f9087d71ca85dde64
bryg217 <bryan.guillen217@gmail.com>
2020-08-03 18:36:16 -0400

    feat: updated structure of component to correclty layout form fields by adding rows and columns; additionally, added import for CreditCardForm.css

--- src/CreditCardForm.jsx
+++ src/CreditCardForm.jsx
@@ -1,44 +1,58 @@
 import React from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Col, Form, Row } from 'react-bootstrap';
+
+import './CreditCardForm.css';
 
 export default function CreditCardForm() {
   return (
-    <Form>
-      <Form.Group controlId="formCcNumber">
-        <Form.Label>Credit Card Number</Form.Label>
-        <Form.Control type="text" placeholder="1234 5678 1234 5678" />
-      </Form.Group>
-      <Form.Group controlId="formCcName">
-        <Form.Label>Credit Card Name</Form.Label>
-        <Form.Control type="text" placeholder="John Smith" />
-      </Form.Group>
-      <Form.Group controlId="formCcExpirationDate">
-        <Form.Label>Expiration Date</Form.Label>
-        <Form.Control as="select" custom>
-          <option>01</option>
-          <option>02</option>
-          <option>03</option>
-          <option>04</option>
-          <option>05</option>
-          <option>06</option>
-          <option>07</option>
-          <option>08</option>
-          <option>09</option>
-          <option>10</option>
-          <option>11</option>
-          <option>12</option>
-        </Form.Control>
-        <Form.Control as="select" custom>
-          <option>2021</option>
-          <option>2022</option>
-          <option>2023</option>
-        </Form.Control>
-      </Form.Group>
-      <Form.Group controlId="formCcCvv">
-        <Form.Label>CVV</Form.Label>
-        <Form.Control type="text" placeholder="123" />
-      </Form.Group>
-      <Button variant="primary" type="submit">Submit</Button>
+    <Form className="credit-card-form">
+      <Form.Row>
+        <Form.Group as={Col} controlId="formCcNumber">
+          <Form.Label>Credit Card Number</Form.Label>
+          <Form.Control type="text" placeholder="1234 5678 1234 5678" />
+        </Form.Group>
+      </Form.Row>
+      <Form.Row>
+        <Form.Group as={Col} controlId="formCcName">
+          <Form.Label>Credit Card Name</Form.Label>
+          <Form.Control type="text" placeholder="John Smith" />
+        </Form.Group>
+      </Form.Row>
+      <Form.Row>
+        <Form.Group as={Col} xs={8} controlId="formCcExpirationDate">
+          <Form.Label>Expiration Date</Form.Label>
+          <Row>
+            <Col>
+              <Form.Control as="select" custom>
+                <option>01</option>
+                <option>02</option>
+                <option>03</option>
+                <option>04</option>
+                <option>05</option>
+                <option>06</option>
+                <option>07</option>
+                <option>08</option>
+                <option>09</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>
+              </Form.Control>
+            </Col>
+            <Col>
+              <Form.Control as="select" custom>
+                <option>2021</option>
+                <option>2022</option>
+                <option>2023</option>
+              </Form.Control>
+            </Col>
+          </Row>
+        </Form.Group>
+        <Form.Group as={Col} xs={4} controlId="formCcCvv">
+          <Form.Label>CVV</Form.Label>
+          <Form.Control type="text" placeholder="123" />
+          </Form.Group>
+      </Form.Row>
+      <Button variant="primary" type="submit" block>Submit</Button>
     </Form>
   );
 }
