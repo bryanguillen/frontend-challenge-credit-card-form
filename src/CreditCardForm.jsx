dda9ce2e2d9320c9d05a23edc0393ed75f67a84b
bryg217 <bryan.guillen217@gmail.com>
2020-08-04 09:52:35 -0400

    refactor: replaced all react-bootstrap components with native HTML/jsx components

--- src/CreditCardForm.jsx
+++ src/CreditCardForm.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Button, Col, Form, Row } from 'react-bootstrap';
 
 import './CreditCardForm.css';
 
@@ -15,56 +14,60 @@ export default function CreditCardForm() {
   }
 
   return (
-    <Form className="credit-card-form" onSubmit={handleSubmit}>
-      <Form.Row>
-        <Form.Group as={Col} controlId="formCcNumber">
-          <Form.Label>Credit Card Number</Form.Label>
-          <input type="text" placeholder="1234 5678 1234 5678" onChange={event => setCreditCardNumber(event.target.value)} />
-        </Form.Group>
-      </Form.Row>
-      <Form.Row>
-        <Form.Group as={Col} controlId="formCcName">
-          <Form.Label>Credit Card Name</Form.Label>
-          <input type="text" placeholder="John Smith" onChange={event => setCreditCardName(event.target.value)} />
-        </Form.Group>
-      </Form.Row>
-      <Form.Row>
-        <Form.Group as={Col} xs={8} controlId="formCcExpirationDate">
-          <Form.Label>Expiration Date</Form.Label>
-          <Row>
-            <Col>
-              <select onChange={event => setCreditCardExpirationMonth(event.target.value)}>
-                <option defaultValue>Month</option>
-                <option>01</option>
-                <option>02</option>
-                <option>03</option>
-                <option>04</option>
-                <option>05</option>
-                <option>06</option>
-                <option>07</option>
-                <option>08</option>
-                <option>09</option>
-                <option>10</option>
-                <option>11</option>
-                <option>12</option>
-              </select>
-            </Col>
-            <Col>
-              <select onChange={event => setCreditCardExpirationYear(event.target.value)}>
-                <option defaultValue>Year</option>
-                <option>2021</option>
-                <option>2022</option>
-                <option>2023</option>
-              </select>
-            </Col>
-          </Row>
-        </Form.Group>
-        <Form.Group as={Col} xs={4} controlId="formCcCvv">
-          <Form.Label>CVV</Form.Label>
-          <input type="text" placeholder="123" onChange={event => setCreditCardCvv(event.target.value)} />
-        </Form.Group>
-      </Form.Row>
+    <form className="credit-card-form" onSubmit={handleSubmit}>
+      <label for="creditCardNumber">Credit Card Number</label>
+      <input
+        name="creditCardNumber"
+        id="creditCardNumber"
+        type="text"
+        placeholder="1234 5678 1234 5678"
+        onChange={event => setCreditCardNumber(event.target.value)}
+      />
+      <label for="creditCardName">Credit Card Name</label>
+      <input
+        name="creditCardName"
+        id="creditCardName"
+        type="text"
+        placeholder="John Smith"
+        onChange={event => setCreditCardName(event.target.value)}
+      />
+      <fieldset>
+        <legend>Expiration Date</legend>
+        <label for="creditCardExpirationMonth" className="hidden-label">Month</label>
+        <select
+          name="creditExpirationMonth"
+          id="creditExpirationMonth"        
+          onChange={event => setCreditCardExpirationMonth(event.target.value)}
+        >
+          <option defaultValue>Month</option>
+          <option>01</option>
+          <option>02</option>
+          <option>03</option>
+          <option>04</option>
+          <option>05</option>
+          <option>06</option>
+          <option>07</option>
+          <option>08</option>
+          <option>09</option>
+          <option>10</option>
+          <option>11</option>
+          <option>12</option>
+        </select>
+        <label for="creditCardExpirationYear" className="hidden-label">Year</label>
+        <select
+          name="creditExpirationYear"
+          id="creditExpirationYear"
+          onChange={event => setCreditCardExpirationYear(event.target.value)}
+        >
+          <option defaultValue>Year</option>
+          <option>2021</option>
+          <option>2022</option>
+          <option>2023</option>
+        </select>
+      </fieldset>
+      <label for="creditCardCvv">CVV</label>
+      <input type="text" placeholder="123" onChange={event => setCreditCardCvv(event.target.value)} />
       <button type="submit">Submit</button>
-    </Form>
+    </form>
   );
 }
