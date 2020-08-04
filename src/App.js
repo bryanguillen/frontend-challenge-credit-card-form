9d99fd83c2d08c6acb0a6d4b1193b50831ca02d6
bryg217 <bryan.guillen217@gmail.com>
2020-08-04 14:21:46 -0400

    feat: added CreditCard component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import CreditCard from './CreditCard';
 import CreditCardForm from './CreditCardForm';
 
 function App() {
   return (
     <div className="App">
+      <CreditCard/>
       <CreditCardForm/>
     </div>
   );
