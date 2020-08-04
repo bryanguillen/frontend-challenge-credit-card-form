8ea0d28244db2ee273d6607f774b60aabb10eaf7
bryg217 <bryan.guillen217@gmail.com>
2020-08-04 11:10:48 -0400

    refactor: removed react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import CreditCardForm from './CreditCardForm';
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <CreditCardForm/>
-      </Container>
+      <CreditCardForm/>
     </div>
   );
 }
