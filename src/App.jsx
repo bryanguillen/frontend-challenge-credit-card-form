1ef24268e97f1814132bceabc7c851d8c97e1408
bryg217 <bryan.guillen217@gmail.com>
2020-08-04 17:38:58 -0400

    refactor: renamed file to have jsx extension for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import CreditCard from './CreditCard';
 import CreditCardForm from './CreditCardForm';
 
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <CreditCard/>
       <CreditCardForm/>
     </div>
