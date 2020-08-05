f69a3341dd287494cddbec8ec6da8756c5115a30
bryg217 <bryan.guillen217@gmail.com>
2020-08-05 13:28:19 -0400

    refactor: added new prop, showFront, which will control what side of the card gets shown via css selectors

--- src/CreditCard.jsx
+++ src/CreditCard.jsx
@@ -5,10 +5,12 @@ import CreditCardFront from './CreditCardFront';
 
 import './CreditCard.css';
 
-export default function CreditCard() {
+export default function CreditCard({ showFront }) {
+  const flipperClasses = showFront ? 'flipper' : 'flipper show-back';
+
   return (
     <div className="credit-card">
-      <div className="flipper">
+      <div className={flipperClasses}>
         <CreditCardFront/>
         <CreditCardBack/>
       </div>
